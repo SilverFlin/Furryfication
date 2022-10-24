3307b73b6959a03d06917b5d05ddbecacfb7a25f
SilverFlin <toledorusso@outlook.com>
2022-10-23 22:59:45 -0700

    feat: validaciones

---
+++ src/auth/Validaciones.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auth;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Toled
+ */
+public class Validaciones {
+    
+    public static boolean validateEmail(String email){
+        // Regex
+        String regex =  "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+        
+        // Compilar regex 
+        Pattern p = Pattern.compile(regex); 
+  
+        // Entrada vacia 
+        if (email == null) return false;  
+  
+        // Generar el Matcher
+        Matcher m = p.matcher(email); 
+  
+        // Return: si coincide con el patron
+        return m.matches();
+    }
+    
+}
